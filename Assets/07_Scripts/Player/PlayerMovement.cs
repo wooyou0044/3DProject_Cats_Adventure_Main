c9b1db84d4f89e8097869d8e702085c118fe770b
wooyou0044 <chawoo2206@gmail.com>
2025-02-24 19:02:18 +0900

    [Feat] Player 동작 추가

--- Assets/07_Scripts/Player/PlayerMovement.cs
+++ Assets/07_Scripts/Player/PlayerMovement.cs
@@ -4,32 +4,86 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Animator animator;
+    IPlayerState currentState;
+
+    public Animator animator { get; set; }
+    public Rigidbody rigid { get; set; }
+    public float facingRight { get; set; }
+    public bool isFloor { get; set; }
+
+    public float turnSpeed = 80.0f;
+    public float moveSpeed = 10.0f;
+    public float jumpHeight = 2.0f;
+
+    [SerializeField] GameObject playerWeapon;
+
+    Rigidbody weaponRigid;
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        weaponRigid = playerWeapon.GetComponent<Rigidbody>();
     }
 
     void Start()
     {
-        
+        rigid.freezeRotation = true;
+        isFloor = true;
+        weaponRigid.isKinematic = true;
+        ChangeState(new IdleState());
     }
 
     void Update()
     {
-        
+        currentState.UpdateState(this);
+
+    }
+
+    private void FixedUpdate()
+    {
+        currentState.FixedUpdateState(this);
     }
 
     public void ChangeState(IPlayerState state)
     {
-
+        currentState = state;
+        currentState.EnterState(this);
     }
 
     public bool CanJump()
     {
-        bool isGround = true;
+        return isFloor;
+    }
 
-        return isGround;
+    public bool CanThrow()
+    {
+        bool isKnifeReturn = true;
+
+        return isKnifeReturn;
+    }
+
+    public void ThrowKnifeAttack()
+    {
+        StartCoroutine(ThrowKnife());
+        //weaponRigid.isKinematic = false;
+        //weaponRigid.gameObject.transform.parent = null;
+        //weaponRigid.AddForce(transform.forward * 10, ForceMode.Impulse);
+    }
+
+    IEnumerator ThrowKnife()
+    {
+        yield return new WaitForSeconds(0.35f);
+        weaponRigid.isKinematic = false;
+        weaponRigid.gameObject.transform.parent = null;
+        weaponRigid.AddForce(transform.forward * 10, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Floor"))
+        {
+            isFloor = true;
+        }
     }
 }
--- Assets/07_Scripts/Player/PlayerState.cs
+++ Assets/07_Scripts/Player/PlayerState.cs
@@ -13,18 +13,29 @@ public class IdleState : IPlayerState
 {
     public void EnterState(PlayerMovement player)
     {
-        player.animator.SetBool("IsRun", false);
+        //player.animator.SetBool("IsRun", false);
     }
 
     public void UpdateState(PlayerMovement player)
     {
-        float h = Input.GetAxis("Horizontal");
+        //float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
 
-        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
-        if(moveDir != Vector3.zero)
+        float moveMagnitude = Mathf.Clamp01(Mathf.Abs(v));
+        player.animator.SetFloat("MoveFloat", moveMagnitude);
+
+        //Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h).normalized;
+        Vector3 moveDir = (Vector3.forward * v).normalized;
+
+        if(r != 0)
         {
-            player.ChangeState(new WalkingState());
+            player.transform.Rotate(Vector3.up * player.turnSpeed * Time.deltaTime * r);
+        }
+
+        if(moveDir.z > 0)
+        {
+            player.ChangeState(new RunningState());
         }
 
         if(Input.GetKeyDown(KeyCode.Space) && player.CanJump())
@@ -32,7 +43,24 @@ public class IdleState : IPlayerState
             player.ChangeState(new JumpingState());
         }
 
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            player.ChangeState(new AttackState());
+        }
 
+        if(Input.GetMouseButtonDown(0) && player.CanThrow())
+        {
+            player.ChangeState(new ThrowKnifeState());
+        }
+
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            // NPC와 대화
+            // 물건 밀기
+            // 물건 잡기
+            // 워프 안에 들어가기
+            // 물건 놓기/던지기
+        }
     }
 
     public void FixedUpdateState(PlayerMovement player)
@@ -40,20 +68,53 @@ public class IdleState : IPlayerState
     }
 }
 
-public class WalkingState : IPlayerState
+public class RunningState : IPlayerState
 {
     public void EnterState(PlayerMovement player)
     {
-        throw new System.NotImplementedException();
+
     }
     public void UpdateState(PlayerMovement player)
     {
-        throw new System.NotImplementedException();
+        //float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
+
+        float moveMagnitude = Mathf.Clamp01(Mathf.Abs(v));
+        player.animator.SetFloat("MoveFloat", moveMagnitude);
+
+        if (r != 0)
+        {
+            player.transform.Rotate(Vector3.up * player.turnSpeed * Time.deltaTime * r);
+        }
+
+        if (v == 0)
+        {
+            player.ChangeState(new IdleState());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && player.CanJump())
+        {
+            player.ChangeState(new JumpingState());
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            player.ChangeState(new AttackState());
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            player.ChangeState(new ThrowKnifeState());
+        }
     }
 
     public void FixedUpdateState(PlayerMovement player)
     {
-        throw new System.NotImplementedException();
+        float v = Input.GetAxis("Vertical");
+        Vector3 moveDir = (Vector3.forward * v);
+
+        player.transform.Translate(moveDir.normalized * player.moveSpeed * Time.deltaTime);
     }
 }
 
@@ -61,18 +122,79 @@ public class JumpingState : IPlayerState
 {
     public void EnterState(PlayerMovement player)
     {
-        throw new System.NotImplementedException();
+        player.animator.SetTrigger("Jump");
+        player.rigid.AddForce(Vector3.up * player.jumpHeight, ForceMode.Impulse);
     }
 
     public void UpdateState(PlayerMovement player)
     {
-        throw new System.NotImplementedException();
+        if(player.rigid.velocity.y <= 0)
+        {
+            player.ChangeState(new IdleState());
+        }
     }
 
     public void FixedUpdateState(PlayerMovement player)
     {
-        throw new System.NotImplementedException();
+        if (player.isFloor == true)
+        {
+            player.isFloor = false;
+        }
     }
 }
 
+public class AttackState : IPlayerState
+{
+    public void EnterState(PlayerMovement player)
+    {
+        player.animator.SetTrigger("NormalAttack");
+    }
+
+    public void UpdateState(PlayerMovement player)
+    {
+        float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
+
+        if(v != 0)
+        {
+            player.ChangeState(new RunningState());
+        }
+        else
+        {
+            player.ChangeState(new IdleState());
+        }
+    }
+
+    public void FixedUpdateState(PlayerMovement player)
+    {
+    }
+}
+
+public class ThrowKnifeState : IPlayerState
+{
+    public void EnterState(PlayerMovement player)
+    {
+        player.animator.SetTrigger("ThrowKnife");
+        player.ThrowKnifeAttack();
+    }
+
+    public void UpdateState(PlayerMovement player)
+    {
+        float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
+
+        if (v != 0)
+        {
+            player.ChangeState(new RunningState());
+        }
+        else
+        {
+            player.ChangeState(new IdleState());
+        }
+    }
+
+    public void FixedUpdateState(PlayerMovement player)
+    {
+    }
+}
 // 공격받으면 체력 UI 뜨는 것 추가 필요
